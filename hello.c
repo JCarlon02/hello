2930996403eed4b7d737dad446240ea56020c21c
Jayden <jayden.carlon13@gmail.com>
2019-08-29 12:35:31 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jayden Carlon
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jayden Carlon\n");
+
+  return 0;
+}
